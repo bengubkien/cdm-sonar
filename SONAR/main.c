d3870c6787a4953771b5b97b8b5d0253b444c430
Benjamín <bengubkien@gmail.com>
2020-12-09 21:59:54 -0300

    Escritura del display e inclusión de las funciones de ella

--- SONAR/main.c
+++ SONAR/main.c
@@ -18,17 +18,29 @@
 #define t_paso 10	
 #define ms_servo 50
 
+// LCD module information
+#define lcd_LineOne     0x00                    // start of line 1
+#define lcd_LineTwo     0x40                    // start of line 2
+
+// LCD instructions
+#define lcd_Clear           0b00000001          // replace all characters with ASCII 'space'
+#define lcd_Home            0b00000010          // return cursor to first position on first line
+#define lcd_EntryMode       0b00000110          // shift cursor from left to right on read/write
+#define lcd_DisplayOff      0b00001000          // turn display off
+#define lcd_DisplayOn       0b00001100          // display on, cursor off, don't blink character
+#define lcd_FunctionReset   0b00110000          // reset the LCD
+#define lcd_FunctionSet8bit 0b00111000          // 8-bit data, 2-line display, 5 x 7 font
+#define lcd_SetCursor       0b10000000          // set cursor position
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <lcd.h>
+#include <math.h>
 
 // Headers de funciones
 void sonar_setup(void);
 void servo_rotation(void);
 void trigger_pulse(void);
-
-int dist_cm;
 
 // Comienzo del main
 int main(void)
@@ -104,6 +116,142 @@ void trigger_pulse(void){
 	PORTL |= (0 << PL1);
 }
 
+/*
+  Nombre:     lcd_init_8d
+  Propósito:  Inicializar el display LCD para una interfaz de 8 bit.
+  Inputs:     Ninguno.
+  Outputs:    Ninguno.
+*/
+
+void lcd_init_8d(void)
+{
+// Delay de encendido.
+    _delay_ms(100);                                 // 40ms.
+
+// Reset del controlador del display LCD.
+    lcd_write_instruction_8d(lcd_functionreset);    // Primera parte de la secuencia de reset.
+    _delay_ms(10);                                  // Delay de 4.1ms.
+
+    lcd_write_instruction_8d(lcd_functionreset);    // Segunda parte de la secuencia de reset.
+    _delay_us(200);                                 // Delay de 100us.
+
+    lcd_write_instruction_8d(lcd_functionreset);    // Tercera parte de la secuencia de reset.
+    _delay_us(200);                                
+
+// Configuración del display LED.
+    lcd_write_instruction_8d(lcd_functionreset);    // Seteo el modo, la cantidad de líneas y font.
+    _delay_us(80);                                  // Delay de 40us.
+
+// Rutina de inicialización (chequear después).
+
+    lcd_write_instruction_8d(lcd_displayoff);       // Apago el display.
+    _delay_us(80);                                  // Delay de 40us.
+
+    lcd_write_instruction_8d(lcd_clear);            // Limpio la RAM del display.
+    _delay_ms(4);                                   // Delay de 1.64ms.
+
+    lcd_write_instruction_8d(lcd_entrymode);        // Seteo el comportamiento del cursor.
+    _delay_us(80);                                  // Delay de 40us.
+
+    lcd_write_instruction_8d(lcd_displayon);        // Prendo el display.
+    _delay_us(80);                                  // Delay de 40us.
+}
+
+/*...........................................................................*/
+
+/*
+  Nombre:     lcd_write_string_8d
+  Propósito:  Mostrar un string en el display LCD.
+  Inputs:     "texto" es el string que se representará en el display.
+  Outputs:    Ninguno.
+*/
+
+void lcd_write_string_8d(uint8_t texto[])
+{
+    volatile int i = 0;                             
+    while (texto[i] != 0)
+    {
+        lcd_write_character_8d(texto[i]);
+        i++;
+        _delay_us(80);                              // Delay de 40us.
+    }
+}
+
+/*...........................................................................*/
+
+/*
+  Nombre:     lcd_write_character_8d
+  Propósito:  Envía un byte de información al registro de datos del display LCD.
+  Inputs:     "datos" es la información a enviar al registro.
+  Outputs:    Ninguno.
+*/
+
+void lcd_write_character_8d(uint8_t datos)
+{
+    lcd_RS_port |= (1<<lcd_RS_bit);                 // Selecciono el registro de datos.
+    lcd_E_port &= ~(1<<lcd_E_bit);                  // Me aseguro que E esté en bajo.
+    lcd_write_8(datos);                             // Escribo los datos.
+}
+
+/*...........................................................................*/
+
+/*
+  Nombre:       lcd_write_instruction_8d
+  Propósito:  Envía un byte de información al registro de instrucciones del display LCD.
+  Inputs:     "instrucciones" es la información a enviar al registro.
+  Outputs:    Ninguno.
+*/
+
+void lcd_write_instruction_8d(uint8_t instrucciones)
+{
+    lcd_RS_port &= ~(1<<lcd_RS_bit);                // Selecciono el registro de instrucciones.
+    lcd_E_port &= ~(1<<lcd_E_bit);                  // Me aseguro que E esté en bajo.
+    lcd_write_8(instrucciones);                     // Escribo las instrucciones.
+}
+
+/*...........................................................................
+  Nombre:     lcd_write_8
+  Propósito:  Envío un byte de información al módulo del LCD.
+  Inputs:     "info" es la información a enviar al registro del LCD.
+  Outputs:    Ninguno.
+*/
+void lcd_write_8(uint8_t info)
+{
+    lcd_D7_port &= ~(1<<lcd_D7_bit);                        // Asumo que el dato es '0'.
+    if (info & 1<<7) lcd_D7_port |= (1<<lcd_D7_bit);        // Fuerzo que el dato sea '1' si es necesario.
+
+    lcd_D6_port &= ~(1<<lcd_D6_bit);                        // Repito para todos los otros bits.
+    if (info & 1<<6) lcd_D6_port |= (1<<lcd_D6_bit);
+
+    lcd_D5_port &= ~(1<<lcd_D5_bit);
+    if (info & 1<<5) lcd_D5_port |= (1<<lcd_D5_bit);
+
+    lcd_D4_port &= ~(1<<lcd_D4_bit);
+    if (info & 1<<4) lcd_D4_port |= (1<<lcd_D4_bit);
+
+    lcd_D3_port &= ~(1<<lcd_D3_bit);
+    if (info & 1<<3) lcd_D3_port |= (1<<lcd_D3_bit);
+
+    lcd_D2_port &= ~(1<<lcd_D2_bit);
+    if (info & 1<<2) lcd_D2_port |= (1<<lcd_D2_bit);
+
+    lcd_D1_port &= ~(1<<lcd_D1_bit);
+    if (info & 1<<1) lcd_D1_port |= (1<<lcd_D1_bit);
+
+    lcd_D0_port &= ~(1<<lcd_D0_bit);
+    if (info & 1<<0) lcd_D0_port |= (1<<lcd_D0_bit);
+
+// Escribo los datos.
+                                                    // 'Address set-up time' (40 nS)
+    lcd_E_port |= (1<<lcd_E_bit);                   // Enable pin high
+    _delay_us(1);                                   // implement 'Data set-up time' (80 nS) and 'Enable pulse width' (230 nS)
+    lcd_E_port &= ~(1<<lcd_E_bit);                  // Enable pin low
+    _delay_us(1);                                   // implement 'Data hold time' (10 nS) and 'Enable cycle time' (500 nS)
+}
+
+/*============================== INTERRUPCIONES ======================*/
+
+
 ISR(TIMER3_OVF_vect){					// Vector de interrupcion del overflow del timer 3
 	trigger_pulse();
 }
@@ -111,7 +259,20 @@ ISR(TIMER3_OVF_vect){					// Vector de interrupcion del overflow del timer 3
 ISR(TIMER4_CAPT_vect)					// Vector de interrupción de input capture para el Timer 4.
 {
 	TCCR4B |= (0<<CS41);				// Freno el timer.
-	dist_cm = ICR4 / (2*58);			// Una cuenta de 2 equivale a 1 us con 8 de prescaler. La cuenta para la distancia en cm es t_us/58 = dist_cm  ==>  count/(2*58) = dist_cm.
+	int dist_cm = ICR4 / (2*58);			// Una cuenta de 2 equivale a 1 us con 8 de prescaler. La cuenta para la distancia en cm es t_us/58 = dist_cm  ==>  count/(2*58) = dist_cm.
 	ICR4 = 0;					// Limpio los registros contadores.
 	TCNT4 = 0;
+	int angulo = floor((OCR1A - t_0grados)*0.088);
+	char string_angulo[9] = "Angulo: ";	
+	//strcat(string_angulo, str(angulo))							// Concateno el string y el ángulo.
+	lcd_write_string_8d(string_angulo)						// Escribo el angulo en el display.
+	
+	if(dist_cm<150) {											// Si el objeto se encuentra a una distancia aceptable...	
+		char string_dist[12] = "Distancia: ";				    // Defino el string para el display.
+		lcd_write_instruction_8d(lcd_SetCursor | lcd_LineTwo);	// Muevo el cursor a la segunda línea.
+		//strcat(string_dist, dist_cm)						
+		lcd_write_string_8d(string_dist, dist_cm)				// Escribo la distancia.
+		lcd_write_instruction_8d(lcd_SetCursor | lcd_LineOne);  // Muevo el cursor de vuelta a la primer línea.
+	}
+	
 }
