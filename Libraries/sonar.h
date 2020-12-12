cb548ed76fee9c4e179a404dac572e5cc3bd8d03
Despaquito <67880023+Despaquito@users.noreply.github.com>
2020-12-12 15:32:07 -0300

    Add files via upload

---
+++ Libraries/sonar.h
@@ -0,0 +1,13 @@
+#define F_CPU 16000000UL
+#define t_0grados 350
+#define t_180grados 2400
+#define t_paso 10
+#define ms_servo 50
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Headers de funciones
+void sonar_setup(void);
+void servo_rotation(void);
+void trigger_pulse(void);
